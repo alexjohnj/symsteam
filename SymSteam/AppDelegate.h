6ebf03503278ceb9598e82266c96fc701af637c5
Alex Jackson <ajamicr@gmail.com>
2012-02-02 20:42:14 +0000

    Initial Commit

---
+++ SymSteam/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SymSteam
+//
+//  Created by Alex Jackson on 02/02/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end

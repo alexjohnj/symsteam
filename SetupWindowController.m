a90499f2346faed46353812cc2dae4d2555a1ab3
Alex Jackson <alex@alexj.me>
2012-03-26 19:33:54 +0100

    Completely rewrote first run procedure. SymSteam will now rename the Local SteamApps folder to 'SteamApps' and the symbolic one to 'SteamAppsSymb' and keep track of these.

--- SetupWindowController.m
+++ SetupWindowController.m
@@ -12,6 +12,9 @@
 @synthesize pathToSymLinkField, pathToNonSymLinkField, continueButton;
 @synthesize symLinkPathProvided, nonSymLinkPathProvided, formComplete;
 
+static NSString * const steamAppsSymbolicLinkPath = @"steamAppsSymbolicLinkPath";
+static NSString * const steamAppsLocalPath = @"steamAppsLocalPath";
+
 #pragma mark - Window Lifecycle methods
 
 - (id)initWithWindow:(NSWindow *)window
@@ -79,11 +82,59 @@
 }
 
 -(IBAction)doneButtonPressed:(id)sender{
+    NSString *providedLocalPath = [[NSString alloc] initWithString:self.pathToNonSymLinkField.stringValue];
+    NSString *providedLocalSymbolicPath = [[NSString alloc] initWithString:self.pathToSymLinkField.stringValue];
+    NSFileManager *fManager = [[NSFileManager alloc] init];
+    
+    NSString *finalLocalPath = [[NSString alloc] init];
+    NSString *finalSymbolicPath = [[NSString alloc] init];
+    
+    if(![providedLocalPath.lastPathComponent isEqualToString:@"SteamApps"]){
+        NSError *moveLocalFolderError;
+        
+        NSURL *newLocalPath = [[NSURL alloc] initFileURLWithPath:providedLocalPath isDirectory:YES];
+        newLocalPath = [newLocalPath URLByDeletingLastPathComponent];
+        newLocalPath = [newLocalPath URLByAppendingPathComponent:@"SteamApps" isDirectory:YES];
+        
+        if(![fManager moveItemAtPath:providedLocalPath toPath:newLocalPath.path error:&moveLocalFolderError]){
+            NSAlert *alert = [NSAlert alertWithMessageText:@"Error Renaming SteamApps Folder" defaultButton:@"Choose New Path" alternateButton:@"Quit" otherButton:nil informativeTextWithFormat:[moveLocalFolderError localizedDescription]];
+            NSInteger result = [alert runModal];
+            if(result != NSOKButton)
+                [[NSApplication sharedApplication] terminate:self];
+        }
+        finalLocalPath = newLocalPath.path;
+    }
+    
+    else{
+        finalLocalPath = providedLocalPath;
+    }
+    
+    if(![providedLocalSymbolicPath.lastPathComponent isEqualToString:@"SteamAppsSymb"]){
+        NSURL *newSymbPath = [[NSURL alloc] initFileURLWithPath:providedLocalSymbolicPath isDirectory:YES];
+        newSymbPath = [newSymbPath URLByDeletingLastPathComponent];
+        newSymbPath = [newSymbPath URLByAppendingPathComponent:@"SteamAppsSymb"];
+        
+        NSError *renameSymbolicFolderError;
+        if (![fManager moveItemAtPath:providedLocalSymbolicPath toPath:newSymbPath.path error:&renameSymbolicFolderError]) {
+            NSAlert *alert = [NSAlert alertWithMessageText:@"Error Renaming SteamApps Symbolic Folder" defaultButton:@"Choose New Path" alternateButton:@"Quit" otherButton:nil informativeTextWithFormat:[renameSymbolicFolderError localizedDescription]];
+            NSInteger result = [alert runModal];
+            if(result != NSOKButton)
+                [[NSApplication sharedApplication] terminate:self];
+        }
+        finalSymbolicPath = newSymbPath.path;  
+    }
+    else{
+        finalSymbolicPath = providedLocalSymbolicPath;
+    }
+    
+    [[NSUserDefaults standardUserDefaults] setValue:finalLocalPath forKey:steamAppsLocalPath];
+    [[NSUserDefaults standardUserDefaults] setValue:finalSymbolicPath forKey:steamAppsSymbolicLinkPath];
     [[NSUserDefaults standardUserDefaults] setValue:[NSNumber numberWithBool:YES] forKey:@"setupComplete"];
-    [[NSUserDefaults standardUserDefaults] setValue:self.pathToNonSymLinkField.stringValue forKey:@"steamAppsLocalPath"];
-    [[NSUserDefaults standardUserDefaults] setValue:self.pathToSymLinkField.stringValue forKey:@"steamAppsSymbolicLinkPath"];
-    [[NSUserDefaults standardUserDefaults] synchronize];
-    [self close];
+    
+    NSAlert *successAlert = [NSAlert alertWithMessageText:@"Success!" defaultButton:@"OK" alternateButton:nil otherButton:nil informativeTextWithFormat:@"Successfully setup SymSteam"];
+    NSInteger result = [successAlert runModal];
+    if(result == NSOKButton)
+        [self close];
 }
 
 -(void)checkPathsProvided{

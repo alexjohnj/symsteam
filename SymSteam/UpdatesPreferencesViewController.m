41a0622606406e0d2b8261e76b589ac24eb3ca3c
Alex Jackson <alex@alexj.me>
2012-04-08 14:21:24 +0100

    Added Sparkle update framework and a new view (and controller) for a software update preference pane.

---
+++ SymSteam/UpdatesPreferencesViewController.m
@@ -0,0 +1,27 @@
+//
+//  UpdatesPreferencesViewController.m
+//  SymSteam
+//
+//  Created by Alex Jackson on 08/04/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "UpdatesPreferencesViewController.h"
+
+@interface UpdatesPreferencesViewController ()
+
+@end
+
+@implementation UpdatesPreferencesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end

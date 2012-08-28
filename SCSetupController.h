794145ee9b29072ee5eac15d6966270c727cd4c9
Alex Jackson <alex@alexj.me>
2012-08-28 15:36:08 +0100

    Refactored SCSetupController a bit more.
    Added 2 new methods. One to extract the path to an external drive from the path to a folder on that drive and one to verify that the disk is a valid steam drive (it just wraps two other methods in the class into one).
    
    In addition, the symbolic link creation method will now check to see if the SteamDrive is connected in order to avoid creating two symbolic SteamApps folders.

--- SCSetupController.h
+++ SCSetupController.h
@@ -7,14 +7,17 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "SCSteamDiskManager.h"
 
 @interface SCSetupController : NSObject
 
 - (DADiskRef)createDADiskFromDrivePath:(NSURL *)drive CF_RETURNS_RETAINED;
 - (BOOL)driveFilesystemIsHFS:(DADiskRef)drive;
 - (NSString *)getDriveUUID:(DADiskRef)drive;
+- (NSURL *)getDrivePathFromFolderPath:(NSURL *)folderPath;
 - (BOOL)createSymbolicLinkToFolder:(NSURL *)folder;
 - (BOOL)folderIsOnExternalDrive:(NSURL *)pathToFolder;
+- (BOOL)verifyProvidedFolderIsUsable:(NSURL *)folder;
 
 //////////////////////////////////////////////////////////////////////
 
--- SCSetupController.m
+++ SCSetupController.m
@@ -32,6 +32,38 @@
     return (pathToFolder.pathComponents.count >= 3 && [pathToFolder.pathComponents[1] isEqualToString:@"Volumes"]);
 }
 
+- (BOOL)verifyProvidedFolderIsUsable:(NSURL *)folder{
+    if(![self folderIsOnExternalDrive:folder]){
+        NSAlert *invalidDestinationAlert = [NSAlert alertWithMessageText:@"Error"
+                                                           defaultButton:@"OK"
+                                                         alternateButton:nil
+                                                             otherButton:nil
+                                               informativeTextWithFormat:@"The folder you provided is not on an external drive."];
+        if([NSApp mainWindow])
+            [invalidDestinationAlert beginSheetModalForWindow:[NSApp mainWindow] modalDelegate:nil didEndSelector:nil contextInfo:NULL];
+        else
+            [invalidDestinationAlert runModal];
+        return NO;
+    }
+    
+    DADiskRef drive = [self createDADiskFromDrivePath:[self getDrivePathFromFolderPath:folder]];
+    if([self getDriveUUID:drive] == NULL){
+        NSAlert *noUUIDFound = [NSAlert alertWithMessageText:@"Error"
+                                               defaultButton:@"OK"
+                                             alternateButton:nil
+                                                 otherButton:nil
+                                   informativeTextWithFormat:@"The drive which the SteamApps folder is on does not have a UUID. Please ensure that the drive is HFS formatted."];
+        if([NSApp mainWindow])
+            [noUUIDFound beginSheetModalForWindow:[NSApp mainWindow] modalDelegate:nil didEndSelector:nil contextInfo:NULL];
+        else
+            [noUUIDFound runModal];
+        CFRelease(drive);
+        return NO;
+    }
+    CFRelease(drive);
+    return YES;
+}
+
 - (NSString *)getDriveUUID:(DADiskRef)drive{
     CFDictionaryRef driveDetails = DADiskCopyDescription(drive);
     
@@ -46,11 +78,27 @@
     }
 }
 
+- (NSURL *)getDrivePathFromFolderPath:(NSURL *)folderPath{
+    if(folderPath == nil){
+        NSLog(@"getDrivePathFromFolderPath: The provided folderPath was nil");
+        return nil;
+    }
+    if(folderPath.pathComponents.count < 3){
+        NSLog(@"getDrivePathFromFolderPath: The provided folderPath was too short to create a drive path");
+        return nil;
+    }
+    return [NSURL fileURLWithPathComponents:(@[folderPath.pathComponents[0], folderPath.pathComponents[1], folderPath.pathComponents[2]])];
+}
+
 - (BOOL)createSymbolicLinkToFolder:(NSURL *)folder{ // Creates a symbolic link at /Application Support/Steam/SteamAppsSymb
     NSFileManager *fManager = [[NSFileManager alloc] init];
     
     NSArray *directories = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDirectory, YES);
-    NSString *symbolicLinkPath = [[directories[0] stringByAppendingPathComponent:@"Steam"]  stringByAppendingPathComponent:@"SteamAppsSymb"];
+    NSString *symbolicLinkPath;
+    if(![[SCSteamDiskManager steamDiskManager] steamDriveIsConnected])
+        symbolicLinkPath = [[directories[0] stringByAppendingPathComponent:@"Steam"]  stringByAppendingPathComponent:@"SteamAppsSymb"];
+    else
+        symbolicLinkPath = [[directories[0] stringByAppendingPathComponent:@"Steam"]  stringByAppendingPathComponent:@"SteamApps"];
     
     if([fManager attributesOfItemAtPath:symbolicLinkPath error:nil] && [[fManager attributesOfItemAtPath:symbolicLinkPath error:nil] fileType] == NSFileTypeSymbolicLink){
         NSAlert *alert = [NSAlert alertWithMessageText:@"A Symbolic Link Already Exists!"

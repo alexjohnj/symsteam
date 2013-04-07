d9f3b85f015ce713c8464e9e380297c3b414b74d
Alex Jackson <alex@alexj.me>
2013-04-07 17:08:15 +0100

    Improved how errors are displayed to the user from notifications.
    Now, a “More info” button will be displayed in error messages that gives the localised failure reason for an error.

--- SymSteam/SCUserNotificationCenterDelegate.m
+++ SymSteam/SCUserNotificationCenterDelegate.m
@@ -17,24 +17,52 @@
 - (void)userNotificationCenter:(NSUserNotificationCenter *)center didActivateNotification:(NSUserNotification *)notification {
     NSDictionary *notificationUserInfo = notification.userInfo;
     if ([notificationUserInfo[@"notificationIdentifier"] isEqualToString:@"errorNotification"]) {
-        NSDictionary *errorDictionary = notificationUserInfo[@"errorDictionary"];
-        NSError *attachedError = [[NSError alloc] initWithDomain:errorDictionary[@"domain"]
-                                                    code:[errorDictionary[@"code"] integerValue]
-                                                userInfo:errorDictionary[@"userInfo"]];
-        [[NSAlert alertWithError:attachedError] runModal];
+        NSError *attachedError = [self createErrorFromDictionary:notificationUserInfo[@"errorDictionary"]];
+        [self displayErrorToUser:attachedError];
     }
 }
 
 - (void)growlNotificationWasClicked:(id)clickContext {
     if([clickContext isKindOfClass:[NSDictionary class]]) {
         if([clickContext[@"notificationIdentifier"] isEqualToString:@"errorNotification"]) {
-            NSDictionary *errorDictionary = clickContext[@"errorDictionary"];
-            NSError *attachedError = [[NSError alloc] initWithDomain:errorDictionary[@"domain"]
-                                                        code:[errorDictionary[@"code"] integerValue]
-                                                    userInfo:errorDictionary[@"userInfo"]];
-            [[NSAlert alertWithError:attachedError] runModal];
+            NSError *attachedError = [self createErrorFromDictionary:clickContext[@"errorDictionary"]];
+            [self displayErrorToUser:attachedError];
         }
     }
+}
+
+#pragma mark - Private Methods
+
+- (void)displayErrorToUser:(NSError *) error{
+    NSAlert *alert = [[NSAlert alloc] init];
+    alert.alertStyle = NSCriticalAlertStyle;
+    NSButton *moreInfoButton = nil;
+    
+    if (error.localizedDescription)
+        alert.messageText = error.localizedDescription;
+    
+    if (error.localizedRecoverySuggestion)
+        alert.informativeText = error.localizedRecoverySuggestion;
+    
+    [alert addButtonWithTitle:NSLocalizedString(@"OK", nil)];
+    
+    if (error.localizedFailureReason) {
+        moreInfoButton = [alert addButtonWithTitle:NSLocalizedString(@"More Information", nil)];
+    }
+
+    NSInteger result = [alert runModal];
+    if (moreInfoButton && result == moreInfoButton.tag) {
+        NSAlert *moreInfoAlert = [NSAlert alertWithMessageText:error.localizedDescription
+                                                 defaultButton:NSLocalizedString(@"OK", nil)
+                                               alternateButton:nil otherButton:nil
+                                     informativeTextWithFormat:@"%@", error.localizedFailureReason];
+        [moreInfoAlert runModal];
+    }
+}
+
+- (NSError *)createErrorFromDictionary:(NSDictionary *)dict {
+    NSError *error = [[NSError alloc] initWithDomain:dict[@"domain"] code:dict[@"code"] userInfo:dict[@"userInfo"]];
+    return error;
 }
 
 @end
